ba43f34203a0c6d67a442fd9dff7d25c029a9a14
Simon Stürz <simon.stuerz@nymea.io>
2025-11-17 12:55:52 +0100

    Update license text and add SPDX identifier

--- libnymea-networkmanager/networkdevice.cpp
+++ libnymea-networkmanager/networkdevice.cpp
@@ -1,152 +1,26 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2020, nymea GmbH
-* Contact: contact@nymea.io
+* Copyright (C) 2013 - 2024, nymea GmbH
+* Copyright (C) 2024 - 2025, chargebyte austria GmbH
 *
-* This file is part of nymea.
-* This project including source code and documentation is protected by
-* copyright law, and remains the property of nymea GmbH. All rights, including
-* reproduction, publication, editing and translation, are reserved. The use of
-* this project is subject to the terms of a license agreement to be concluded
-* with nymea GmbH in accordance with the terms of use of nymea GmbH, available
-* under https://nymea.io/license
+* This file is part of libnymea-networkmanager.
 *
-* GNU Lesser General Public License Usage
-* Alternatively, this project may be redistributed and/or modified under the
-* terms of the GNU Lesser General Public License as published by the Free
-* Software Foundation; version 3. This project is distributed in the hope that
-* it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* libnymea-networkmanager is free software: you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public License
+* as published by the Free Software Foundation, either version 3
+* of the License, or (at your option) any later version.
+*
+* libnymea-networkmanager is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 * Lesser General Public License for more details.
 *
 * You should have received a copy of the GNU Lesser General Public License
-* along with this project. If not, see <https://www.gnu.org/licenses/>.
+* along with libnymea-networkmanager. If not, see <https://www.gnu.org/licenses/>.
 *
-* For any further details and any questions please contact us under
-* contact@nymea.io or see our FAQ/Licensing Information on
-* https://nymea.io/license/faq
-*
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/*!
-    \class NetworkDevice
-    \brief Represents the base class of a network device.
-    \inmodule nymea-networkmanager
-    \ingroup networkmanager
-
-
-*/
-
-/*! \enum NetworkDevice::NetworkDeviceState
-    \value NetworkDeviceStateUnknown
-    \value NetworkDeviceStateUnmanaged
-    \value NetworkDeviceStateUnavailable
-    \value NetworkDeviceStateDisconnected
-    \value NetworkDeviceStatePrepare
-    \value NetworkDeviceStateConfig
-    \value NetworkDeviceStateNeedAuth
-    \value NetworkDeviceStateIpConfig
-    \value NetworkDeviceStateIpCheck
-    \value NetworkDeviceStateSecondaries
-    \value NetworkDeviceStateActivated
-    \value NetworkDeviceStateDeactivating
-    \value NetworkDeviceStateFailed
-*/
-
-/*! \enum NetworkDevice::NetworkDeviceStateReason
-    \value NetworkDeviceStateReasonNone
-    \value NetworkDeviceStateReasonUnknown
-    \value NetworkDeviceStateReasonNowManaged
-    \value NetworkDeviceStateReasonNowUnmanaged
-    \value NetworkDeviceStateReasonConfigFailed
-    \value NetworkDeviceStateReasonIpConfigUnavailable
-    \value NetworkDeviceStateReasonIpConfigExpired
-    \value NetworkDeviceStateReasonNoSecrets
-    \value NetworkDeviceStateReasonSupplicantDisconnected
-    \value NetworkDeviceStateReasonSupplicantConfigFailed
-    \value NetworkDeviceStateReasonSupplicantFailed
-    \value NetworkDeviceStateReasonSupplicantTimeout
-    \value NetworkDeviceStateReasonPppStartFailed
-    \value NetworkDeviceStateReasonPppDisconnected
-    \value NetworkDeviceStateReasonPppFailed
-    \value NetworkDeviceStateReasonDhcpStartFailed
-    \value NetworkDeviceStateReasonDhcpError
-    \value NetworkDeviceStateReasonDhcpFailed
-    \value NetworkDeviceStateReasonSharedStartFailed
-    \value NetworkDeviceStateReasonSharedFailed
-    \value NetworkDeviceStateReasonAutoIpStartFailed
-    \value NetworkDeviceStateReasonAutoIpError
-    \value NetworkDeviceStateReasonAutoIpFailed
-    \value NetworkDeviceStateReasonModemBusy
-    \value NetworkDeviceStateReasonModemNoDialTone
-    \value NetworkDeviceStateReasonModemNoCarrier
-    \value NetworkDeviceStateReasonModemDialTimeout
-    \value NetworkDeviceStateReasonModemDialFailed
-    \value NetworkDeviceStateReasonModemInitFailed
-    \value NetworkDeviceStateReasonGsmApnFailed
-    \value NetworkDeviceStateReasonGsmRegistrationNotSearching
-    \value NetworkDeviceStateReasonGsmRegistrationDenied
-    \value NetworkDeviceStateReasonGsmRegistrationTimeout
-    \value NetworkDeviceStateReasonGsmRegistrationFailed
-    \value NetworkDeviceStateReasonGsmPinCheckFailed
-    \value NetworkDeviceStateReasonFirmwareMissing
-    \value NetworkDeviceStateReasonRemoved
-    \value NetworkDeviceStateReasonSleeping
-    \value NetworkDeviceStateReasonConnectionRemoved
-    \value NetworkDeviceStateReasonUserRequest
-    \value NetworkDeviceStateReasonCarrier
-    \value NetworkDeviceStateReasonConnectionAssumed
-    \value NetworkDeviceStateReasonSupplicantAvailable
-    \value NetworkDeviceStateReasonModemNotFound
-    \value NetworkDeviceStateReasonBtFailed
-    \value NetworkDeviceStateReasonGsmSimNotInserted
-    \value NetworkDeviceStateReasonGsmSimPinRequired
-    \value NetworkDeviceStateReasonGsmSimPukRequired
-    \value NetworkDeviceStateReasonGsmSimWrong
-    \value NetworkDeviceStateReasonInfinibandMode
-    \value NetworkDeviceStateReasonDependencyFailed
-    \value NetworkDeviceStateReasonBR2684Failed
-    \value NetworkDeviceStateReasonModemManagerUnavailable
-    \value NetworkDeviceStateReasonSsidNotFound
-    \value NetworkDeviceStateReasonSecondaryConnectionFailed
-    \value NetworkDeviceStateReasonDcbFoecFailed
-    \value NetworkDeviceStateReasonTeamdControlFailed
-    \value NetworkDeviceStateReasonModemFailed
-    \value NetworkDeviceStateReasonModemAvailable
-    \value NetworkDeviceStateReasonSimPinIncorrect
-    \value NetworkDeviceStateReasonNewActivision
-    \value NetworkDeviceStateReasonParentChanged
-    \value NetworkDeviceStateReasonParentManagedChanged
-*/
-
-
-/*! \enum NetworkDevice::NetworkDeviceType
-    \value NetworkDeviceTypeUnknown
-    \value NetworkDeviceTypeEthernet
-    \value NetworkDeviceTypeWifi
-    \value NetworkDeviceTypeBluetooth
-    \value NetworkDeviceTypeOlpcMesh
-    \value NetworkDeviceTypeWiMax
-    \value NetworkDeviceTypeModem
-    \value NetworkDeviceTypeInfiniBand
-    \value NetworkDeviceTypeBond
-    \value NetworkDeviceTypeVLan
-    \value NetworkDeviceTypeAdsl
-    \value NetworkDeviceTypeBridge
-    \value NetworkDeviceTypeGeneric
-    \value NetworkDeviceTypeTeam
-    \value NetworkDeviceTypeTun
-    \value NetworkDeviceTypeIpTunnel
-    \value NetworkDeviceTypeMacVLan
-    \value NetworkDeviceTypeVXLan
-    \value NetworkDeviceTypeVEth
-*/
-
-
-/*! \fn void NetworkDevice::deviceChanged();
-    This signal will be emitted when the properties of this \l{NetworkDevice} have changed.
-*/
-
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "networkdevice.h"
 
